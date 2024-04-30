4d9de413bad1e09f07b6ecde886749334554de1c
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-04-30 13:19:50 +0700

    initial no sh

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const queryClient = new QueryClient()
 
 export default function App() {
+  const [data, setData] = useState([])
+
+  const checkApi = async () => {
+    const res = await fetch('https://next.api.whathebyte.com/posts')
+    if (!res.ok) {
+      throw new Error("Gabisa pak")
+    }
+
+    const dd = await res.json()
+    setData(() => dd.data)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Button title='Click me' onPress={checkApi} />
+        <StatusBar style="auto" />
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Text>{item.title}</Text>}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    </QueryClientProvider>
   );
 }
 
